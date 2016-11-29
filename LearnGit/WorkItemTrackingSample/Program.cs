ec55e9f80ce296298744faed66af62c19dab00f9
jingzhu yan (Shang Hai Wei Chuang Ruan Jian) <v-yjingz@microsoft.com>
2016-11-29 13:44:05 +0800

    new change 11-29

--- LearnGit/WorkItemTrackingSample/Program.cs
+++ LearnGit/WorkItemTrackingSample/Program.cs
@@ -11,7 +11,7 @@ namespace WorkItemTrackingSample
             {
                 sum += i;
             }
-            Console.WriteLine("sum:" + sum);
+            Console.WriteLine("try to sum:" + sum);
         }
     }
 }
